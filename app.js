32e1bd4fe5b3784bdc08fb393ed9941379f1dc6d
rednoah <reinhard.pointner@gmail.com>
2015-04-07 22:11:47 +0800

    * more prototyping

--- app.js
+++ app.js
@@ -6,38 +6,49 @@ var querystring = require('querystring')
 var child_process = require('child_process')
 fs = require('fs')
 
-var PORT = 5000
+var PORT = 5452
 
-var storage = {}
+var runningProcesses = {}
+
+function getLogFile(n) {
+    return './log/' + n + '.log'
+}
+
+function spawnChildProcess(options) {
+    var t_start = Date.now()
+    var logFileName = getLogFile(t_start)
+
+    var process = child_process.spawn('filebot',
+        ['-script', 'dev:sysenv', '--output', options.output],
+        {stdio: ['ignore', fs.openSync(logFileName, 'a'), fs.openSync(logFileName, 'a')]}
+    )
+
+    runningProcesses[t_start] = {pid: process.pid, t: t_start, log: logFileName, exitCode: null, options: options}
+
+    process.on('close', function (code) {
+        runningProcesses[t_start].pid = null
+        runningProcesses[t_start].exitCode = code
+    })
+
+    return t_start
+}
 
 http.createServer(function (req, res) {
     var requestParameters = url.parse(req.url)
-    console.log(requestParameters.pathname);
+
     if ('/filebot/execute' == requestParameters.pathname) {
         var options = querystring.parse(requestParameters.query)
-        var id = Date.now().toString()
-        var logFileName = './log/'+id+'.log'
+        spawnChildProcess(options)
 
-        storage[id] = {t: id, log: logFileName, exitCode: null}
-
-        var process = child_process.spawn('filebot',
-            ['-version', '--output', options.output],
-            {stdio: [ 'ignore', fs.openSync(logFileName, 'a'), fs.openSync(logFileName, 'a') ]}
-        )
-        process.on('close', function (code) {
-            storage[id].exitCode = code
-            console.log(storage)
-        })
-
-        var result = {status:'OK', t: id, pid: process.pid}
+        var result = {status: 'OK', runningProcesses: runningProcesses}
 
         res.writeHead(200, {'Content-Type': 'text/json'});
         res.end(JSON.stringify(result))
         return
     }
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(requestParameters.toString());
+    res.writeHead(404, {'content-type': 'text/plain'})
+    res.end('Illegal Request.')
 }).listen(PORT, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:' + PORT + '/');
