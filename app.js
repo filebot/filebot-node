1a97279cf6c3679d0f1052cbed2fce3befea9562
rednoah <reinhard.pointner@gmail.com>
2015-04-08 22:16:51 +0800

    * added /log viewing endpoint

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-process.title = 'filebot-nos'
+// INCLUDES
 
 var http = require('http')
 var url = require('url')
@@ -6,12 +6,20 @@ var querystring = require('querystring')
 var child_process = require('child_process')
 fs = require('fs')
 
+// CONFIGURATION AND GLOBAL VARIABLES
+
 var PORT = 5452
+var HOST = '127.0.0.1'
 
-var runningProcesses = []
+var ACTIVE_TASKS = []
+var LOG_FOLDER = './log'
 
-function getLogFile(n) {
-    return './log/' + n + '.log'
+
+// HELPER FUNCTIONS
+
+
+function getLogFile(id) {
+    return LOG_FOLDER + '/' + id + '.log'
 }
 
 function getCommand() {
@@ -34,12 +42,21 @@ function getCommandArguments(options) {
 
 function spawnChildProcess(command, arguments) {
     var id = Date.now()
-    var pd = {pid: null, t: id, log: getLogFile(id), exitCode: null, duration: null, command: command, arguments: arguments}
+    var logFile = getLogFile(id)
+
+    var pd = {
+        pid: null,
+        t: id,
+        exitCode: null,
+        duration: null,
+        command: command,
+        arguments: arguments
+    }
 
     var process = child_process.spawn(
         command,
         arguments,
-        {stdio: ['ignore', fs.openSync(pd.log, 'a'), fs.openSync(pd.log, 'a')]}
+        {stdio: ['ignore', fs.openSync(logFile, 'a'), fs.openSync(logFile, 'a')]}
     )
     pd.pid = process.pid
 
@@ -54,22 +71,83 @@ function spawnChildProcess(command, arguments) {
     return pd
 }
 
-http.createServer(function (req, res) {
-    var requestParameters = url.parse(req.url)
 
-    if ('/filebot/execute' == requestParameters.pathname) {
-        var options = querystring.parse(requestParameters.query)
-        var pd = spawnChildProcess(getCommand(), getCommandArguments(options))
+// ROUTES
 
-        var result = {status: 'OK', process: pd}
+function execute(requestParameters) {
+    var options = querystring.parse(requestParameters.query)
+    var pd = spawnChildProcess(getCommand(), getCommandArguments(options))
 
-        res.writeHead(200, {'Content-Type': 'text/json'});
-        res.end(JSON.stringify(result))
-        return
+    ACTIVE_TASKS.push(pd)
+
+    return pd
+}
+
+function listTasks(requestParameters) {
+    return ACTIVE_TASKS
+}
+
+function listLogs(requestParameters) {
+    return fs.readdirSync(LOG_FOLDER).map(function (s) {
+        return s.substr(0, s.lastIndexOf('.'))
+    })
+}
+
+function getLogContent(id) {
+    return fs.readFileSync(getLogFile(id), 'UTF-8')
+}
+
+function handleRequest(requestParameters) {
+    var requestPath = requestParameters.pathname
+
+    if ('/execute' == requestPath) {
+        return execute(requestParameters)
     }
 
-    res.writeHead(404, {'content-type': 'text/plain'})
-    res.end('Illegal Request.')
-}).listen(PORT, '127.0.0.1');
+    if ('/tasks' == requestPath) {
+        return listTasks(requestParameters)
+    }
 
-console.log('Server running at http://127.0.0.1:' + PORT + '/');
+    if ('/logs' == requestPath) {
+        return listLogs(requestParameters)
+    }
+
+    if (/\/logs\/\w+/.test(requestPath)) {
+        return getLogContent(requestPath.match(/\/logs\/(\w+)/)[1])
+    }
+
+    return null
+}
+
+
+// START NODE SERVER
+
+
+process.title = 'filebot-nos'
+
+
+http.createServer(function (req, res) {
+    var requestParameters = url.parse(req.url)
+    var result = null
+    var status = 200
+
+    try {
+        // try to process request
+        result = handleRequest(requestParameters)
+
+        // or report failure otherwise
+        if (result == null) {
+            result = {status: 'ERROR', error: 'ILLEGAL REQUEST'}
+            status = 400
+        }
+    } catch (error) {
+        result = {status: 'ERROR', error: error.toString()}
+        status = 500
+    }
+
+    res.writeHead(status, {'Content-Type': 'text/json'})
+    res.end(JSON.stringify(result))
+}).listen(PORT, HOST);
+
+
+console.log('FileBot Node Server running at http://' + HOST + ':' + PORT + '/version');
