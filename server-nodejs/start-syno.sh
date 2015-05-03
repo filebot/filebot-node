fd33696ca1d7415c275d16e93b8df9f98e8ca844
rednoah <reinhard.pointner@gmail.com>
2015-05-03 08:50:04 +0800

    * create startup files for test config and synology config

--- server-nodejs/start-syno.sh
+++ server-nodejs/start-syno.sh
@@ -11,10 +11,10 @@ export FILEBOT_NODE_HTTPS_PORT='5453'
 export FILEBOT_NODE_HTTPS_KEY='/usr/syno/etc/ssl/ssl.key/server.key'
 export FILEBOT_NODE_HTTPS_CERT='/usr/syno/etc/ssl/ssl.crt/server.crt'
 
-export FILEBOT_NODE_CLIENT='../client-extjs/build/production/FileBot'
-export FILEBOT_EXECUTABLE='filebot'
+export FILEBOT_NODE_CLIENT='/usr/syno/synoman/webman/3rdparty/filebot-node'
+export FILEBOT_EXECUTABLE='/var/packages/filebot/target/filebot.sh'
 
 # --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
 # --max_executable_size (max size of executable memory (in Mbytes))
 # --use_idle_notification (Use idle notification to reduce memory footprint.)
-node --max_executable_size=16 --optimize_for_size --use_idle_notification ./app.js
+node --max_executable_size=16 --optimize_for_size --use_idle_notification app.js
---
+++ server-nodejs/start-test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export FILEBOT_NODE_HOST='127.0.0.1'
+export FILEBOT_NODE_AUTH='NONE'
+
+export FILEBOT_NODE_HTTP='YES'
+export FILEBOT_NODE_HTTP_PORT='5452'
+
+export FILEBOT_NODE_CLIENT='../client'
+export FILEBOT_EXECUTABLE='filebot'
+
+# --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
+# --max_executable_size (max size of executable memory (in Mbytes))
+# --use_idle_notification (Use idle notification to reduce memory footprint.)
+node --max_executable_size=16 --optimize_for_size --use_idle_notification app.js
