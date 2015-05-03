dbdcca76c7dae1515a34816a48eeb5a2bd31fdab
rednoah <reinhard.pointner@gmail.com>
2015-05-04 04:37:31 +0800

    * simplify paths

--- package/synology/package/server/start.sh
+++ package/synology/package/server/start.sh
@@ -11,13 +11,13 @@ export FILEBOT_NODE_HTTPS_PORT='5453'
 export FILEBOT_NODE_HTTPS_KEY='/usr/syno/etc/ssl/ssl.key/server.key'
 export FILEBOT_NODE_HTTPS_CERT='/usr/syno/etc/ssl/ssl.crt/server.crt'
 
-export FILEBOT_NODE_CLIENT='/usr/local/filebot-node/client' 
+export FILEBOT_NODE_CLIENT='client' 
 export FILEBOT_EXECUTABLE='/usr/bin/filebot'
 
 # set working dir
-cd '/usr/local/filebot-node/server'
+cd '/usr/local/filebot-node'
 
 # --max_executable_size (max size of executable memory (in Mbytes))
 # (NOT YET SUPPORTED SYNO NODE.JS) --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
 # --use_idle_notification (Use idle notification to reduce memory footprint.)
-/usr/bin/node --max_executable_size=16 --use_idle_notification app.js
+/usr/bin/node --max_executable_size=16 --use_idle_notification 'server/app.js'
