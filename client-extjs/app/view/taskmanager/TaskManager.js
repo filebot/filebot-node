2a3b8309df491b66f4877a0d9afd9e225341eef8
rednoah <reinhard.pointner@gmail.com>
2015-05-05 07:14:02 +0800

    * immediatley download full log (without opening a new tab)

--- client-extjs/app/view/taskmanager/TaskManager.js
+++ client-extjs/app/view/taskmanager/TaskManager.js
@@ -20,7 +20,7 @@ Ext.define('FileBot.view.taskmanager.TaskManager', {
         type: 'print',
         callback: function() {
             var url = FileBot.Node.getLogAllEndpoint()
-            window.open(url)
+            window.open(url, '_self') // _self -> immediatley download full log (without opening a new tab)
         }
     }],
     
@@ -92,7 +92,7 @@ Ext.define('FileBot.view.taskmanager.TaskManager', {
         }]
     }],
 
-    title: 'Task Manager',
+    title: 'Tasks',
     sortableColumns: false,
     enableColumnHide: false,
     enableColumnMove: false,
