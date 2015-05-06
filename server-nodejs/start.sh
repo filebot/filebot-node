d12debcd11ff6a0a315fabf1764ef7996b173f3b
rednoah <reinhard.pointner@gmail.com>
2015-05-06 22:37:24 +0800

    * update test parameters

--- server-nodejs/start.sh
+++ server-nodejs/start.sh
@@ -6,10 +6,14 @@ export FILEBOT_NODE_AUTH='NONE'
 export FILEBOT_NODE_HTTP='YES'
 export FILEBOT_NODE_HTTP_PORT='5452'
 
-export FILEBOT_NODE_CLIENT='../client'
-export FILEBOT_EXECUTABLE='filebot'
+export FILEBOT_CMD='filebot'
+export FILEBOT_CMD_CWD="$PWD"
+export FILEBOT_CMD_UID=`id -u $USER`
+export FILEBOT_CMD_GID=`id -g $USER`
+
+export FILEBOT_NODE_CLIENT='../dist/generic/client'
 
 # --max_executable_size (max size of executable memory (in Mbytes))
 # --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
 # --use_idle_notification (Use idle notification to reduce memory footprint.)
-node --max_executable_size=16 --optimize_for_size --use_idle_notification app.js
+node --max_executable_size=16 --optimize_for_size --use_idle_notification 'app.js'
