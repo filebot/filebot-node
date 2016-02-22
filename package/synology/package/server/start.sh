5388e2bb42d03bdd46fe65381849faa90b9de16b
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-02-23 01:56:28 +0800

    set -Xmx to 0.7 of physical memory

--- package/synology/package/server/start.sh
+++ package/synology/package/server/start.sh
@@ -11,15 +11,18 @@ export FILEBOT_NODE_HTTPS_PORT="5453"
 export FILEBOT_NODE_HTTPS_KEY="/usr/syno/etc/ssl/ssl.key/server.key"
 export FILEBOT_NODE_HTTPS_CRT="/usr/syno/etc/ssl/ssl.crt/server.crt"
 
-# set user
-export USER="admin"
+
+export USER="admin"                                                                           # set admin as filebot user
+export JAVA_OPTS=`free | awk -vm=1024 -vp=0.7 '/Mem:/ {printf "-Xmx%dm", ($2*p)/m; exit}'`    # set -Xmx to 0.7 of physical memory
+
 
 export FILEBOT_CMD="filebot"
 export FILEBOT_CMD_CWD="$SYNOPKG_PKGDEST_VOL"
 export FILEBOT_CMD_UID=`id -u $USER`
-export FILEBOT_CMD_GID=`cat /etc/group | grep 'administrators' | cut -d: -f3` # cannot use `id -u $USER` because the result is 100:users but we need 101:administrators because users don't have execute permissions
+export FILEBOT_CMD_GID=`cat /etc/group | grep 'administrators' | cut -d: -f3`                 # cannot use `id -u $USER` because the result is 100:users but we need 101:administrators because users don't have execute permissions
 
-export FILEBOT_NODE_CLIENT="" # serve client-side code via DSM only
+export FILEBOT_NODE_CLIENT=""                                                                 # serve client-side code via DSM only
+
 
 # set working dir
 cd "$SYNOPKG_PKGDEST"
