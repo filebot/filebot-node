a526bd8020082c8bbfcf91aa4a575e8e2c580cb3
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-02-23 00:18:12 +0800

    --optimize_for_size seems to be working now

--- package/synology/package/server/start.sh
+++ package/synology/package/server/start.sh
@@ -25,6 +25,6 @@ export FILEBOT_NODE_CLIENT="" # serve client-side code via DSM only
 cd "$SYNOPKG_PKGDEST"
 
 # --max_executable_size (max size of executable memory (in Mbytes))
-# (NOT YET SUPPORTED SYNO NODE.JS) --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
+# --optimize_for_size (Enables optimizations which favor memory size over execution speed.)
 # --use_idle_notification (Use idle notification to reduce memory footprint.)
-node --max_executable_size=16 --use_idle_notification "server/app.js"
+node --max_executable_size=16 --optimize_for_size --use_idle_notification "server/app.js"
