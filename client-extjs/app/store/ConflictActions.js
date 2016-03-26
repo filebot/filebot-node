3bb1209952e8a133e55c26f8163ac85b88ab7cd4
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-03-26 17:42:30 +0800

    Support --conflict index

--- client-extjs/app/store/ConflictActions.js
+++ client-extjs/app/store/ConflictActions.js
@@ -11,7 +11,7 @@ Ext.define('FileBot.store.ConflictActions', {
         [0, 'skip', 'Skip existing files'],
         [1, 'override', 'Override existing files'],
         [2, 'auto', 'Override if new file is better'],
-        [3, 'index', 'Keep both and add sequence number'],
+        [3, 'index', 'Keep both and index new file'],
         [4, 'fail', 'Fail if files already exist']
     ]
 });
