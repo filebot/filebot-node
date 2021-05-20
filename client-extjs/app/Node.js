a9dc84f0516a6262950da03cd89748c99e6e337a
Reinhard Pointner <reinhard.pointner@gmail.com>
2021-05-20 21:36:15 +0700

    Generate correct url in CGI mode

--- client-extjs/app/Node.js
+++ client-extjs/app/Node.js
@@ -124,7 +124,7 @@ Ext.define('FileBot.Node', {
             const id = request.id
             const command = request.command
 
-            const url = new URL(this.getPostEndpoint("task?id=" + id), window.location).href
+            const url = new URL(this.getPostEndpoint("task", {id: id}), window.location).href
             const curl = request.curl + ' "' + url + '"\n'
 
             Ext.MessageBox.show({
