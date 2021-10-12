3751614e9c478c0bf2d3c04b7e44be6e2cbc3a84
Reinhard Pointner <rednoah@filebot.net>
2021-10-13 00:13:04 +0700

    PIPESTATUS requires /bin/bash

--- server-nodejs/task.sh
+++ server-nodejs/task.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash -u
 
 export FILEBOT_NODE_DATA="$PWD/data"
 export FILEBOT_NODE_TASK="$1"
 
+
 # import user environment
-source "$FILEBOT_NODE_DATA/environment.sh"
+if [ -f "$FILEBOT_NODE_DATA/environment.sh" ]; then
+	source "$FILEBOT_NODE_DATA/environment.sh"
+fi
+
 
 # execute filebot task and record output
 filebot "@$FILEBOT_NODE_DATA/task/$FILEBOT_NODE_TASK.args" 2>&1 | tee -a "$FILEBOT_NODE_DATA/log/$FILEBOT_NODE_TASK.log"
 
-exit ${PIPESTATUS[0]} # return filebot exit code (and not tee exit code)
+
+# get filebot exit code (and not tee exit code)
+STATUS="${PIPESTATUS[0]}"
+
+# treat ExitCode.NOOP as ExitCode.SUCCESS
+if [ $STATUS -eq 100 ]; then
+	exit 0
+fi
+
+exit $STATUS
