a74d1b3c65706695bb9c0d3ee5f36e455b283e4a
Reinhard Pointner <rednoah@filebot.net>
2023-08-17 15:16:49 +0700

    Add Help and Fullscreen window button

--- package/synology-dsm7/target/client/FileBot.NodeClient.js
+++ package/synology-dsm7/target/client/FileBot.NodeClient.js
@@ -31,6 +31,19 @@ Ext.define("FileBot.NodeClient.AppWindow", {
 					height: '100%',
 					frameborder: '0'
 				}
+			}],
+			tools: [{
+				id:'fullscreen',
+				qtip: 'Open in New Tab',
+				handler: function(event, element, panel) {
+					window.open('/webman/3rdparty/filebot-node/index.html', '_blank')
+				}
+			}, {
+				id:'help',
+				qtip: 'Open Help',
+				handler: function(event, element, panel) {
+					window.open('https://www.filebot.net/syno/support.html', '_blank')
+				}
 			}]
 		}, config);
 
